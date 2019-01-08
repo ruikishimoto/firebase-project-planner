75cc23f65e90529fb866f11185e2b632cbfcbfb0
SaiKumar Immadi <immadisaikumar@ymail.com>
2019-01-08 13:54:48 +0530

    adds background and some other important tweaks

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
-        <Link to="/" className="brand-logo">
+        <Link to="/" className="brand-logo left">
           MarioPlan
         </Link>
         <SignedInLinks />
