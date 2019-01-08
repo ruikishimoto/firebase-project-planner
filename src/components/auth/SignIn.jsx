e8eb1133a5d2ff81f72e7fc7d8d63fefb06660af
SaiKumar Immadi <immadisaikumar@ymail.com>
2019-01-08 13:29:33 +0530

    adds signup

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 
 class SignIn extends Component {
+  state = {
+    email: "",
+    password: ""
+  };
+
   handleChange = e => {
-    console.log(e);
+    this.setState({
+      [e.target.id]: e.target.value
+    });
   };
 
   handleSubmit = e => {
     e.preventDefault();
-    console.log(e);
+    console.log(this.state);
   };
 
   render() {
